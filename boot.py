83b388f6e928ffc767f5a7ff022dcd7be4b7af13
earlMasagaLogimine <earl.masaga@logimine.com>
2024-03-08 11:00:15 +0800

    turn green

--- boot.py
+++ boot.py
@@ -8,8 +8,8 @@ from neopixel import NeoPixel
 pin = Pin(48, Pin.OUT)
 pixel = NeoPixel(pin, 1)
 
-pixel[0] = (255,0,0) #red led
+pixel[0] = (0,255,0) #green led
 pixel.write()
-print('[LED] display red')
+print('[LED] display green')
 
 ugit.pull_all()
