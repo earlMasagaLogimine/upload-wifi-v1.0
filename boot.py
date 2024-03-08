33ec07774ad5e8fd8cf446516933c797b6db30cb
earlMasagaLogimine <earl.masaga@logimine.com>
2024-03-08 10:55:21 +0800

    display red

--- boot.py
+++ boot.py
@@ -1,5 +1,15 @@
 # boot
 
 import ugit
+from machine import Pin
+from time import sleep
+from neopixel import NeoPixel
+
+pin = Pin(48, Pin.OUT)
+pixel = NeoPixel(pin, 1)
+
+pixel[0] = (255,0,0) #red led
+pixel.write()
+print('[LED] display red')
 
 ugit.pull_all()
